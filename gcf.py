5282b0b4e430ba6532c4a16bf2e1a5f2e03932bb
leah-81 <leahw76@nycstudents.net>
2024-02-26 12:30:47 -0500

    vds

--- gcf.py
+++ gcf.py
@@ -1,8 +1,10 @@
-number_1 = int(input("give me a number:"))
+""" number_1 = int(input("give me a number:"))
 number_2 = int(input("give me a second number"))
 for i in range (1, number_1+1):
     if number_1%i == 0 and number_2%i == 0:
         factors = [i]
 else:
         factors.append
-        print (factors[-1])
+        print (factors[-1]) """
+
+number = int(input("Insert a number"))
